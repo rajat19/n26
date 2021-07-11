6aa0b5d946b11cd7109359e52c6121875a5bf48f
evilj0ker <rajatsri94@gmail.com>
2021-07-11 18:34:16 +0530

    add statistics model test

--- src/test/java/com/n26/model/StatisticsModelTest.java
+++ src/test/java/com/n26/model/StatisticsModelTest.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 public class StatisticsModelTest {
 
@@ -18,15 +20,19 @@ public class StatisticsModelTest {
         statistics1.setAvg(BigDecimal.ONE);
 
         Statistics statistics2 = new Statistics();
-        statistics2.setSum(BigDecimal.ONE);
-        statistics2.setCount(1);
-        statistics2.setMax(BigDecimal.ONE);
+        statistics2.setSum(BigDecimal.valueOf(11));
+        statistics2.setCount(2);
+        statistics2.setMax(BigDecimal.TEN);
         statistics2.setMin(BigDecimal.ONE);
-        statistics2.setAvg(BigDecimal.ONE);
+        statistics2.setAvg(BigDecimal.valueOf(5.5));
 
         Statistics result = new Statistics();
         result.addStatistics(statistics1);
         result.addStatistics(statistics2);
-        System.out.println(result);
+        assertEquals(3, result.getCount());
+        assertEquals("12.00", result.getSum().toString());
+        assertEquals("4.00", result.getAvg().toString());
+        assertEquals("10.00", result.getMax().toString());
+        assertEquals("1.00", result.getMin().toString());
     }
 }
